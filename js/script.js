37ce415ad135e7c1052ab357a054aa2f92acef12
Adrian Taylor <33820513+Hadrianix@users.noreply.github.com>
2020-10-22 08:30:14 -0600

    changed

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function error() {
+    
+  
+
+        var animation = bodymovin.loadAnimation({
+            container: document.getElementById('no-parts'),
+            renderer: 'svg',
+            loop: false,
+            autoplay: true,
+            path: '/js/noparts.json'
+        });
+        
+      
+   
+
+}
